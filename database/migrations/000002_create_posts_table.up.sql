69a87a012380ac7da5c22283f24a0d8e8d8e596d
Just-Ven <alnurbaisar@gmail.com>
2026-04-02 23:44:12 +0500

    added: goose migrations

--- database/migrations/000002_create_posts_table.up.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists posts (
-    id serial primary key,
-    author_id int not null references users(id),
-    content text not null,
-    created_at timestamptz not null default now(),
-    updated_at timestamptz not null default now()
-);
---
+++ database/migrations/20260402183514_create_posts.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+create table if not exists posts (
+     id serial primary key,
+     author_id int not null references users(id),
+     content text not null,
+     created_at timestamptz not null default now(),
+     updated_at timestamptz not null default now()
+);
+
+-- +goose Down
+drop table if exists posts;
